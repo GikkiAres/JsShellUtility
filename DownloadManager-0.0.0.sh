d38ca89da52a0b5eddc78606585ad430b6ba29e1
GikkiAres <GikkiAres@icloud.com>
2022-11-14 17:10:10 +0800

    update

--- DownloadManager-0.0.0.sh
+++ DownloadManager-0.0.0.sh
@@ -1,35 +1,41 @@
 # 判断文件夹是否存在且非空
 function isDirNotEmpty() {
-    dirPath=$1
-    if [[ ! -e "${dirPath}" ]]; then
-        echo "dir exists"
-        return 0;
+    dstPath=$1
+    if [[ ! -e "${dstPath}" ]]; then
+        echo "dir not exists"
+        return 0
     fi
-    info=`ls ${dirPath}`
+    info=$(ls ${dstPath})
     if [[ "${info}" = "" ]]; then
         echo "dir is empty"
-        return 0;
+        return 0
     fi
-    return 1;
+    return 1
 }
 
+# $1 压缩包下载地址
+# $2 压缩包存放路径
+# $3 解压后的存放路径
+#
 function downloadZipIfNeeded() {
     # 压缩包下载地址.
     zipDownloadUrl=$1
     # 文件压缩包名称
     zipPath=$2
     # 解压后的文件夹目录
-    dirPath=$3
-    dirTopPath=`dirname ${dirPath}`
-    
-    if [[ ! -e "${dirPath}" ]]; then
-        if [[ ! -e "${zipPath}" ]]; then
-            curl -o "${zipPath}" "${zipDownloadUrl}"
-            [[ $? != 0 ]] && echo "download failed" && exit
-        fi
-        tar zxvf ${zipPath}  -C "${dirTopPath}"
-        [[ $? != 0 ]] && echo "uncompress failed" && exit
-    else
-        echo "file: ${dirPath} existed."
+    dstPath=$3
+    # dstPath的上层dirPath
+    dstDirPath=$(dirname ${dstPath})
+
+    # 判断文件夹是否存在且非空,否则继续.
+    isDirNotEmpty ${dstPath}
+    [[ $? = 1 ]] && echo "file: ${dstPath} existed." && return 0
+    # if [[ ! -e "${dstPath}" ]]; then
+    # fi?
+    if [[ ! -e "${zipPath}" ]]; then
+        curl -o "${zipPath}" "${zipDownloadUrl}"
+        [[ $? != 0 ]] && echo "download failed" && exit
     fi
+    tar zxvf ${zipPath} -C "${dstDirPath}"
+    [[ $? != 0 ]] && echo "uncompress failed" && exit
 }
