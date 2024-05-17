133b78293e21c4193f7d23bbabaaf12855ad889b
GikkiAres <ybg@coremail.cn>
2024-05-17 13:37:43 +0800

    # [0.0.0.2] [2024-05-17] 1,更新了每日合并脚本,获取scriptAbsolutePath的逻辑优化.

--- 02-application/daily_merge.sh
+++ 02-application/daily_merge.sh
@@ -2,14 +2,32 @@
 
 # projectDir="/Users/gikkiares/Desktop/software_project_hierarchy_b/01-stu/01-project_structure/02-node/01-ts/02-server_starter-gap/ga_ts_server_starter_a-p"
 # filePath="${projectDir}/src/configs/Config.ts"
+# scriptPath是绝对路径时 不需要拼接cwd
+# 是相对路径 才需要拼接cwd
+# BASH_SOURCE 是命令中使用的脚本的路径,并不是脚本的绝对路径.
 cwd=`pwd`
-scriptPath=$cwd/$BASH_SOURCE
-scriptsDir=`dirname scriptPath`
-projectDir=`dirname scriptsDir`
-#filePath=package.json
+# 转换为linux风格路径,如果是/开头是绝对路径.
+scriptPath=`cygpath $BASH_SOURCE`
 echo "scriptPath:${scriptPath}"
-filePath=$projectDir/AppScope/app.json5
+scriptAbsolutePath=$scriptPath
 
+if [[ $scriptPath != "/"* ]]; then
+  echo "not absulote path"
+  scriptAbsolutePath=$cwd/$scriptPath
+fi
+
+scriptsDir=`dirname $scriptAbsolutePath`
+projectDir=`dirname $scriptsDir`
+#filePath=package.json
+echo "scriptsDir:${scriptsDir}"
+echo "projectDir:${projectDir}"
+
+filePath=$projectDir/AppScope/app.json5
+echo "filePath:${filePath}"
+[[ ! -e $filePath ]] && echo "file not exist" && read && exit
+
+# 切换到项目目录
+cd $projectDir
 
 function updateBuildByAwk {
     filePath=$1
@@ -66,7 +84,6 @@ function setBuildBySed {
 
 function main() {
   echo "welcome to use [DailyMergeSystem]"
-  [[ ! -e $filePath ]] && echo "file not exist"
 
   # 切换到 master 并检查是新的commitid.
   git checkout master
@@ -74,7 +91,8 @@ function main() {
   currentMasterId=`git log -1 --pretty=format:%h`
   lastMasterId=`cat scripts/master_last_commit_id`
 
-  [[ "${currentMasterId}" == "${lastMasterId}" ]] && echo "current:${currentMasterId},last:${lastMasterId},master is update to date" && exit
+  [[ "${currentMasterId}" == "${lastMasterId}" ]] && echo "current:${currentMasterId},last:${lastMasterId},master is update to date" && read && exit
+  echo "current:${currentMasterId},last:${lastMasterId},master has new commit,prepare to merge"
 
   # 切换到release分支
   git checkout build/release
@@ -91,7 +109,13 @@ function main() {
   git commit -m "[DailyMergeSystem] [update build ${currentBuild}]"
   git push
 
+  echo ${currentMasterId} >  scripts/master_last_commit_id
+
+  #读取一个字符,让界面停留
+  read 
+#  > /dev/null
   echo "done."
+
 }
 
 main
